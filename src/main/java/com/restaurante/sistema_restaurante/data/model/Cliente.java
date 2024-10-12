905f4706590df9e4db077e67b4a1c4f253e7bb9a
Rosyane Iris <rosymiguel201@gmail.com>
2024-10-12 11:31:41 -0300

    arrumei o bug do mysql que não estava rodando

--- src/main/java/com/restaurante/sistema_restaurante/data/model/Cliente.java
+++ src/main/java/com/restaurante/sistema_restaurante/data/model/Cliente.java
@@ -1,4 +1,19 @@
 package com.restaurante.sistema_restaurante.data.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "tb_cliente")
+@Data
 public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+    private Long telefone;
+    private Long dateofbirth;
 }
