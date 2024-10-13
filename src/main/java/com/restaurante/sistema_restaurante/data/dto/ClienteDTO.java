62847958b68ea1e6d03f591f5525343468856964
Rosyane Iris <rosymiguel201@gmail.com>
2024-10-12 21:35:12 -0300

    criação dos DTO's de cliente

---
+++ src/main/java/com/restaurante/sistema_restaurante/data/dto/ClienteDTO.java
@@ -0,0 +1,16 @@
+package com.restaurante.sistema_restaurante.data.dto;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+public class ClienteDTO {
+
+    private Long id;
+    private String name;
+    private String email;
+    private Long login;
+    private Long password;
+}
---
+++ src/main/java/com/restaurante/sistema_restaurante/data/dto/ClienteLowDTO.java
@@ -0,0 +1,10 @@
+package com.restaurante.sistema_restaurante.data.dto;
+
+import lombok.Data;
+
+@Data
+public class ClienteLowDTO {
+
+    private Long id;
+    private String name;
+}
