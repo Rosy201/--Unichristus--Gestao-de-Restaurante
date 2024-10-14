5780c84a70a1d1521a0cf8e7a222e7177f5814d9
Rosyane Iris <rosymiguel201@gmail.com>
2024-10-14 18:43:49 -0300

    Adicionando as anotações para o swagger

--- src/main/java/com/restaurante/sistema_restaurante/controller/ClienteController.java
+++ src/main/java/com/restaurante/sistema_restaurante/controller/ClienteController.java
@@ -3,6 +3,9 @@ package com.restaurante.sistema_restaurante.controller;
 import com.restaurante.sistema_restaurante.data.dto.ClienteDTO;
 import com.restaurante.sistema_restaurante.data.dto.ClienteLowDTO;
 import com.restaurante.sistema_restaurante.service.ClienteService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,26 +18,71 @@ public class ClienteController {
     @Autowired
     private ClienteService clienteservice;
 
+    @Operation(summary = "Cadastra os dados de um cliente | role: [ADMIN]",
+                        tags = "Cliente")
+
+    @ApiResponses({
+
+            @ApiResponse(responseCode = "200", description = "Usuario cadastrado com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Exception não tratada")
+
+    })
     @PostMapping
     public ClienteDTO create(@RequestBody ClienteDTO cliente) {
         return clienteservice.save(cliente);
     }
 
+    @Operation(summary = "Atualiza os dados do cliente",
+            tags = "ClienteAtualiza")
+
+    @ApiResponses({
+
+            @ApiResponse(responseCode = "200", description = "Usuario atualizado com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Usuario não encontrado")
+
+    })
     @PutMapping
     public ClienteDTO update(@RequestBody ClienteDTO cliente) {
         return clienteservice.save(cliente);
     }
 
+    @Operation(summary = "Encontra os dados do cliente",
+            tags = "ClienteProcurar")
+
+    @ApiResponses({
+
+            @ApiResponse(responseCode = "200", description = "Usuario encontrado com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Usuario não encontrado")
+
+    })
     @GetMapping("/all")
     public List<ClienteLowDTO> findAll() {
         return clienteservice.findAll();
     }
 
+    @Operation(summary = "Deleta os dados do cliente",
+            tags = "ClienteDeletado")
+
+    @ApiResponses({
+
+            @ApiResponse(responseCode = "200", description = "Usuario deletado com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Usuario não encontrado")
+
+    })
     @DeleteMapping("/{id}")
     public void delete(@PathVariable Long id) {
         clienteservice.delete(id);
     }
 
+    @Operation(summary = "Encontra os dados do cliente",
+            tags = "ClienteAllDados")
+
+    @ApiResponses({
+
+            @ApiResponse(responseCode = "200", description = "Usuario encontrado com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Usuario não encontrado")
+
+    })
     @GetMapping("/{id}")
     public ClienteDTO findById(@PathVariable Long id) {
         return clienteservice.findById(id);
