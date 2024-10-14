d6e571d45dfe9ef940412b58e2755d582d75c88b
Rosyane Iris <rosymiguel201@gmail.com>
2024-10-14 14:36:58 -0300

    Adicionando a ferramenta Swagger

--- src/main/java/com/restaurante/sistema_restaurante/SistemaRestauranteApplication.java
+++ src/main/java/com/restaurante/sistema_restaurante/SistemaRestauranteApplication.java
@@ -3,6 +3,22 @@ package com.restaurante.sistema_restaurante;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Sistema Restaurante",
+				version = "1.0",
+				description = "Descrição da minha API",
+				contact = @Contact(
+						name = "Rosyane Iris",
+						email = "rosyaneiris201@gmail.com",
+						url = "https://github.com/Rosy201")
+
+
+		)
+)
 @SpringBootApplication
 public class SistemaRestauranteApplication {
 
