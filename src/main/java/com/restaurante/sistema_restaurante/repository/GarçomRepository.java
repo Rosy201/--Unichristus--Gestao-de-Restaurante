5982da187f8c1aad5740aecb5bd9dc72dd0ff207
Rosyane Iris <rosymiguel201@gmail.com>
2024-10-21 12:55:33 -0300

    criação do repositorio garçom

---
+++ src/main/java/com/restaurante/sistema_restaurante/repository/GarçomRepository.java
@@ -0,0 +1,7 @@
+package com.restaurante.sistema_restaurante.repository;
+
+import com.restaurante.sistema_restaurante.data.model.Garçom;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GarçomRepository extends JpaRepository<Garçom ,Long> {
+}
