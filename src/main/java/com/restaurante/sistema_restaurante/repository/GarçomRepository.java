78ef7188de9f9514bcae6e6be74b431c0891895c
Rosyane Iris <rosymiguel201@gmail.com>
2024-10-28 18:40:11 -0300

    criação de prato e anotações

--- src/main/java/com/restaurante/sistema_restaurante/repository/GarçomRepository.java
+++
@@ -1,7 +0,0 @@
-package com.restaurante.sistema_restaurante.repository;
-
-import com.restaurante.sistema_restaurante.data.model.Garçom;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GarçomRepository extends JpaRepository<Garçom ,Long> {
-}
